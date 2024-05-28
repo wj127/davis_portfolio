48b122d45546342c9e9d1d9ea56e864c13a4bd11
Davis Y <wj_legolas@hotmail.es>
2024-05-28 11:53:31 +0200

    [Feature Add]: console fake buttons + other stuff

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,8 +4,18 @@ import { TerminalCursor } from './components/terminal-cursor';
 export default function Home() {
   return (
     <div className={styles.mainDiv}>
-      {/*<p className={styles.terminalText} />*/}
-      <TerminalCursor />
+      <div className={styles.consoleWrapper}>
+        <div className={styles.consoleTop}>
+          <div className={styles.buttonContainer}>
+            <div id={styles.buttonCancel} className={styles.button}/>
+            <div id={styles.buttonMinimize} className={styles.button}/>
+            <div id={styles.buttonMaximize} className={styles.button}/>
+          </div>
+        </div>
+        <div className={styles.consoleTextWrapper}>
+          <TerminalCursor />
+        </div>
+      </div>
     </div>
   );
 }
