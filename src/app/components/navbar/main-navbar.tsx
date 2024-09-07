0b8fe55243cc01eaae1dceb112c88c96fad06295
wj127 <davis.armas.ayala@gmail.com>
2024-09-07 16:54:36 +0100

    [Feature Add]: adding collapsible navbar for web

--- src/app/components/navbar/main-navbar.tsx
+++ src/app/components/navbar/main-navbar.tsx
@@ -1,32 +1,48 @@
 'use client';
 
-import React from 'react';
+import React, { useState } from 'react';
 import Link from 'next/link';
 import * as NavigationMenu from '@radix-ui/react-navigation-menu';
 import NavBarStyles from '@/app/components/navbar/main-navbar.module.scss';
 import { usePathname } from 'next/navigation';
 
-export const MainNavbar: React.FC = () => (
-  <NavigationMenu.Root orientation='horizontal' className={NavBarStyles.NavigationMenuRoot}>
-    <NavigationMenu.List className={NavBarStyles.NavigationMenuList}>
-      <NavigationMenu.Item>
-        <CustomLink href='/'>Home</CustomLink>
-      </NavigationMenu.Item>
+export const MainNavbar: React.FC = () => {
+  const [isOpen, setIsOpen] = useState(false);
 
-      <NavigationMenu.Item>
-        <CustomLink href='/cv'>CV</CustomLink>
-      </NavigationMenu.Item>
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
 
-      <NavigationMenu.Item>
-        <CustomLink href='/work-experience'>Work Experience</CustomLink>
-      </NavigationMenu.Item>
+  return (
+    <div className={NavBarStyles.NavbarContainer}>
+      <NavigationMenu.Root
+        orientation='horizontal'
+        className={`${NavBarStyles.NavigationMenuRoot} ${isOpen ? NavBarStyles.Open : ''}`}
+      >
+        <NavigationMenu.List className={NavBarStyles.NavigationMenuList}>
+          <NavigationMenu.Item>
+            <CustomLink href='/'>Home</CustomLink>
+          </NavigationMenu.Item>
 
-      <NavigationMenu.Item>
-        <CustomLink href='/blog'>Blog</CustomLink>
-      </NavigationMenu.Item>
-    </NavigationMenu.List>
-  </NavigationMenu.Root>
-);
+          <NavigationMenu.Item>
+            <CustomLink href='/cv'>CV</CustomLink>
+          </NavigationMenu.Item>
+
+          <NavigationMenu.Item>
+            <CustomLink href='/work-experience'>Work Experience</CustomLink>
+          </NavigationMenu.Item>
+
+          <NavigationMenu.Item>
+            <CustomLink href='/blog'>Blog</CustomLink>
+          </NavigationMenu.Item>
+        </NavigationMenu.List>
+      </NavigationMenu.Root>
+      <button className={NavBarStyles.ToggleButton} onClick={toggleMenu} aria-expanded={isOpen}>
+        {isOpen ? '▲' : '▼'}
+      </button>
+    </div>
+  );
+};
 
 const CustomLink: React.FC<React.ComponentProps<typeof Link>> = ({ href, ...props }) => {
   const pathname = usePathname();
